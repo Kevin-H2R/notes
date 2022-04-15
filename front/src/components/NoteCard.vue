bc424b2b1848d8df2606dc9159ebd7ef9671dafc
Kevin <herrbach.kevin@gmail.com>
2022-04-15 16:50:52 +0900

    Adds note card

---
+++ front/src/components/NoteCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card width="500">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text>{{ content }}</v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "NoteCard",
+  props: {
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
+    date: {
+      type: String,
+    },
+  },
+};
+</script>
